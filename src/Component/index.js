3b98db6f91e4062bfb8d938839ce5d7f357f1fcb
hanjunho <cloudtubekr@naver.com>
2020-12-15 13:29:36 +0900

    Board

--- src/Component/index.js
+++ src/Component/index.js
@@ -1,2 +1,3 @@
 export { default as Todolist } from './Todolist/Todolist';
 export { default as TodolistLocal } from './TodolistLocal/Todolist';
+export { default as Board } from './Board/BoardPan';
